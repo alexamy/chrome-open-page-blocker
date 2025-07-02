213e79073a33eadb0efb8f82eb272dad3260b6d3
Aleksandr Chashchin <ac@happynumbers.com>
2025-07-02 16:03:50 +0200

    add ts

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,8 @@
+import typescript from '@rollup/plugin-typescript';
+
 /** @type {import('rollup').RollupOptions} */
 export default {
+  plugins: [typescript()],
   input: [
     'src/service-worker/service-worker.ts',
     'src/content/content.ts',
