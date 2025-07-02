5f8c6cd59e7d136a44ca7044ba7dabe4c94be7ad
Aleksandr Chashchin <ac@happynumbers.com>
2025-07-02 16:34:15 +0200

    add shadow root

--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -1,1 +1,16 @@
 console.log('This is a popup script!');
+
+class SiteRow extends HTMLElement {
+  constructor() {
+    super();
+
+    const shadowRoot = this.attachShadow({ mode: 'open' });
+
+    const root = document.createElement('div');
+    root.append('Hello!');
+
+    shadowRoot.appendChild(root);
+  }
+}
+
+window.customElements.define('site-row', SiteRow);
