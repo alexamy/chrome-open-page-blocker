c48f891dab951c8d720e3122ccded412778aa87c
Aleksandr Chashchin <achasch@gmail.com>
2025-07-02 23:53:08 +0200

    add cases

---
+++ src/popup/elements/SiteRows.test.ts
@@ -0,0 +1,15 @@
+import { describe, it } from 'vitest';
+
+function setup() {}
+
+it('renders with child elements', () => {});
+it('adds new row when button is clicked', () => {});
+
+describe('emits data', () => {
+  it('when created', () => {});
+  it('when row is added', () => {});
+  it('when row is removed', () => {});
+  it('when row is checked', () => {});
+  it('when row is changed', () => {});
+  it('only for checked rows with value', () => {});
+});
