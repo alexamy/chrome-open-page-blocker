7eba6985f6d0e3413d3d23c75daa323fccbaa02e
Aleksandr Chashchin <ac@happynumbers.com>
2025-07-02 15:52:03 +0200

    add c;pse

--- src/service-worker/service-worker.ts
+++ src/service-worker/service-worker.ts
@@ -1,11 +1,24 @@
 console.log('Service worker loaded!');
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  console.log('Received message in popup:', request);
+  console.log('Received message:', request);
 
   if (request.type === 'current-url') {
     const url = request.url;
     console.log('Received URL from content script:', url);
+
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      if (tabs.length > 0) {
+        const activeTab = tabs[0];
+        console.log('Active tab:', activeTab);
+
+        if (activeTab.id) {
+          chrome.tabs.remove(activeTab.id);
+        }
+      } else {
+        console.warn('No active tab found.');
+      }
+    });
   }
 
   return false;
