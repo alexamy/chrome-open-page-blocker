9f9c00b67c7af8ce49865f09eb5ff94db333231e
Aleksandr Chashchin <ac@happynumbers.com>
2025-07-02 18:42:02 +0200

    add style

--- src/popup/SiteRows.ts
+++ src/popup/SiteRows.ts
@@ -1,7 +1,27 @@
 export class SiteRows extends HTMLElement {
   constructor() {
     super();
+
+    const shadowRoot = this.attachShadow({ mode: 'open' });
+    const style = document.createElement('style');
+    style.textContent = `
+      .container {
+        display: flex;
+        flex-direction: column;
+      }
+    `;
+
+    const container = document.createElement('div');
+    container.classList.add('container');
+
+    const slot = document.createElement('slot');
+
+    container.appendChild(slot);
+    shadowRoot.appendChild(style);
+    shadowRoot.appendChild(container);
   }
+
+  connectedCallback() {}
 }
 
 window.customElements.define('site-rows', SiteRows);
