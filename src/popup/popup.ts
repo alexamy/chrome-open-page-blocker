7b2b99cb3aaadfad4b393021bf6a64e48c11aa54
Aleksandr Chashchin <achasch@gmail.com>
2025-07-02 22:42:39 +0200

    listen

--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -1,2 +1,7 @@
 import './SiteRow';
 import './SiteRows';
+
+const root = document.querySelector('site-rows');
+
+// @ts-expect-error
+root?.addEventListener('site-rows:data', (e) => console.log(e.detail));
