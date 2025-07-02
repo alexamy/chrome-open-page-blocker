be056244fc7452492cd22ea2e9e103bed4128dbe
Aleksandr Chashchin <ac@happynumbers.com>
2025-07-02 18:37:53 +0200

    refactor

--- src/popup/SiteRows.ts
+++ src/popup/SiteRows.ts
@@ -2,28 +2,6 @@ export class SiteRows extends HTMLElement {
   constructor() {
     super();
   }
-
-  connectedCallback() {
-    const shadowRoot = this.attachShadow({ mode: 'open' });
-
-    const style = document.createElement('style');
-    style.textContent = `
-      .container {
-        display: flex;
-        flex-direction: column;
-      }
-    `;
-
-    const container = document.createElement('div');
-    const slot = document.createElement('slot');
-
-    this.childNodes.forEach((node) => container.appendChild(node));
-
-    container.appendChild(slot);
-
-    shadowRoot.appendChild(style);
-    shadowRoot.appendChild(container);
-  }
 }
 
 window.customElements.define('site-rows', SiteRows);
--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -1,1 +1,2 @@
-console.log('Hello from popup!');
+import './SiteRow';
+import './SiteRows';
