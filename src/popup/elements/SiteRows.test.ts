de9ecb039615b1e61d99eafb33c46058f90ef445
Aleksandr Chashchin <ac@happynumbers.com>
2025-07-03 14:09:19 +0200

    add render

--- src/popup/elements/SiteRows.test.ts
+++ src/popup/elements/SiteRows.test.ts
@@ -1,5 +1,8 @@
-import { describe, it, vi } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
+import { screen } from 'shadow-dom-testing-library';
 import { SiteRowElement } from './SiteRow';
+import './SiteRow';
+import './SiteRows';
 
 function setup(children: SiteRowElement[] = []) {
   const root = document.createElement('site-rows');
@@ -12,7 +15,18 @@ function setup(children: SiteRowElement[] = []) {
   return { root, children, onData };
 }
 
-it('renders with child elements', () => {});
+it('renders with child elements', () => {
+  const child1 = document.createElement('site-row') as SiteRowElement;
+  child1.append('youtube.com');
+
+  const child2 = document.createElement('site-row') as SiteRowElement;
+  child2.append('pinterest.com');
+
+  const element = setup([child1, child2]);
+
+  expect(screen.getByShadowText('youtube.com')).toBeInTheDocument();
+  expect(screen.getByShadowText('pinterest.com')).toBeInTheDocument();
+});
 
 it('adds and focus new row when button is clicked', () => {});
 
