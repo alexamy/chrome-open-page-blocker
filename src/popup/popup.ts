eb506fdfa11fcea9cb3337673ca3c59f2301cf40
Aleksandr Chashchin <ac@happynumbers.com>
2025-07-03 17:51:55 +0200

    add popup

--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -1,6 +1,6 @@
 import './elements/SiteRow';
 import './elements/SiteRows';
-import { SiteRowsElement } from './elements/SiteRows';
+import { SiteRowsDataEntry, SiteRowsElement } from './elements/SiteRows';
 
 const root = document.querySelector('site-rows') as SiteRowsElement;
 
@@ -8,4 +8,22 @@ if (!root) {
   throw new Error('Site rows root element is not found!');
 }
 
-root.addEventListener('site-rows:data', (e) => console.log(e.detail));
+root.addEventListener('site-rows:data', (e) => {
+  console.log('set!');
+  chrome.storage.sync.set({
+    'site-rows-storage': e.detail,
+  });
+});
+
+chrome.storage.sync.get(['site-rows-storage']).then((data) => {
+  console.log('get!', data);
+  const entries = data['site-rows-storage'] as SiteRowsDataEntry[];
+  if (entries) {
+    entries.forEach((entry) => {
+      const element = document.createElement('site-row');
+      element.setAttribute('checked', entry.checked.toString());
+      element.append(entry.value);
+      root.appendChild(element);
+    });
+  }
+});
