159d25fd3cd49de52c5409f4303c63fbdfa560ef
Aleksandr Chashchin <ac@happynumbers.com>
2025-07-03 17:56:40 +0200

    remove log

--- src/content/content.ts
+++ src/content/content.ts
@@ -1,8 +1,5 @@
 import { type Message } from '../types';
 
-console.log('Content script loaded!');
-
 // Send the current URL to the background script
 const url = window.location.href;
 chrome.runtime.sendMessage({ type: 'current-url', url } satisfies Message);
-console.log('Current URL:', url);
