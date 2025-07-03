3e00d3df3a9d6b561a53e9070289946ac22f027a
Aleksandr Chashchin <ac@happynumbers.com>
2025-07-03 18:51:52 +0200

    remove content script

--- src/content/content.ts
+++
@@ -1,7 +0,0 @@
-import { type Message } from '../types';
-
-// Send the current URL to the background script
-chrome.runtime.sendMessage({
-  type: 'current-url',
-  url: window.location.href,
-} satisfies Message);
