69b0f35e03b4413d61f1a5aaa6a19f09107c5887
Aleksandr Chashchin <ac@happynumbers.com>
2025-07-03 18:22:22 +0200

    remove content export

--- src/content/content.ts
+++ src/content/content.ts
@@ -1,10 +1,9 @@
 import { type Message } from '../types';
 
 // Send the current URL to the background script
-sendUrl((url) =>
-  chrome.runtime.sendMessage({ type: 'current-url', url } satisfies Message)
-);
+chrome.runtime.sendMessage({
+  type: 'current-url',
+  url: window.location.href,
+} satisfies Message);
 
-export function sendUrl(onUrl: (url: string) => void) {
-  onUrl(window.location.href);
-}
+// WARN: No exports or chrome will fail!
