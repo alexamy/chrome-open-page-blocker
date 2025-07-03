0f635b73ee4705ed2eac8ae530f43aa81614f7a5
Aleksandr Chashchin <ac@happynumbers.com>
2025-07-03 18:56:28 +0200

    refactor

--- src/service-worker/service-worker.ts
+++ src/service-worker/service-worker.ts
@@ -3,52 +3,29 @@ import { STORAGE_KEY } from '../types';
 
 // TODO: needs testing
 
-// local blocklist
-const block = prepareBlockList();
-
 start();
 
-// setup
+// first start
 async function start() {
   const storage = await chrome.storage.sync.get(STORAGE_KEY);
   const entries: SiteRowsDataEntry[] = storage[STORAGE_KEY] ?? [];
-  block.reassign(entries);
+  updateRules(entries);
 }
 
 // if storage is changed
-chrome.storage.onChanged.addListener((changes, namespace) => {
+chrome.storage.onChanged.addListener((changes) => {
   const entries: SiteRowsDataEntry[] = changes[STORAGE_KEY]?.newValue ?? [];
-  block.reassign(entries);
+  updateRules(entries);
 });
 
-// page web request
-chrome.webRequest.onBeforeRequest.addListener(
-  (details) => {
-    const url = details.url;
-    const path = url.replace(/^https?\:\/\//, '').replace(/^www\./, '');
-    const result = block.list.has(path) ? { cancel: true } : {};
+function updateRules(entries: SiteRowsDataEntry[]) {
+  const blocklist = getBlocklist(entries);
+}
 
-    return result;
-  },
-  { urls: ['<all_urls>'] },
-  ['blocking']
-);
+function getBlocklist(entries: SiteRowsDataEntry[]) {
+  const filtered = entries
+    .filter((entry) => entry.checked)
+    .map((entry) => entry.value);
 
-function prepareBlockList() {
-  let list = new Set();
-
-  function reassign(entries: SiteRowsDataEntry[]) {
-    const filtered = entries
-      .filter((entry) => entry.checked)
-      .map((entry) => entry.value);
-
-    list = new Set(...filtered);
-  }
-
-  return {
-    reassign,
-    get list() {
-      return list;
-    },
-  };
+  return filtered;
 }
