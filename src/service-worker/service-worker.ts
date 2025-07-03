880dbacecfd80fe2ec97a4c6b5e1ad2c36aba795
Aleksandr Chashchin <ac@happynumbers.com>
2025-07-03 18:04:22 +0200

    add close path

--- src/service-worker/service-worker.ts
+++ src/service-worker/service-worker.ts
@@ -1,28 +1,41 @@
+import { SiteRowsDataEntry } from '../popup/elements/SiteRows';
 import { type Message } from '../types';
 
-console.log('Service worker loaded!');
+// TODO: needs testing
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   console.log('Received message:', request);
   request = request as Message;
 
   if (request.type === 'current-url') {
-    const url = request.url;
-    console.log('Received URL from content script:', url);
+    const url = request.url as string;
+    const path = url.replace(/^https?\:\/\//, '');
 
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      if (tabs.length > 0) {
-        const activeTab = tabs[0];
-        console.log('Active tab:', activeTab);
+    chrome.storage.sync.get(['site-rows-storage']).then((data) => {
+      const entries = (data['site-rows-storage'] as SiteRowsDataEntry[]) ?? [];
+      const shouldClose = entries.some(
+        (entry) => entry.checked && path.startsWith(entry.value)
+      );
 
-        if (activeTab.id) {
-          // chrome.tabs.remove(activeTab.id);
-        }
-      } else {
-        console.warn('No active tab found.');
+      if (shouldClose) {
+        closeCurrentTab();
       }
     });
   }
 
   return false;
 });
+
+function closeCurrentTab() {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    if (tabs.length > 0) {
+      const activeTab = tabs[0];
+
+      if (activeTab.id) {
+        chrome.tabs.remove(activeTab.id);
+      }
+    } else {
+      console.warn('No active tab found.');
+    }
+  });
+}
