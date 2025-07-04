81f6918c899e50b10e72bab2358e48c4bd166dff
Aleksandr Chashchin <ac@happynumbers.com>
2025-07-04 14:29:05 +0200

    add storage etc

--- src/service-worker/service-worker.ts
+++ src/service-worker/service-worker.ts
@@ -2,28 +2,55 @@ import { SiteRowsDataEntry } from '../popup/elements/SiteRows';
 import { STORAGE_KEY } from '../types';
 
 // TODO: needs testing
+const blacklist = makeBlacklist();
 
-start();
+// load data on startup
+blacklist.reload();
 
-// setup
-async function start() {
-  const storage = await chrome.storage.sync.get(STORAGE_KEY);
-  const entries: SiteRowsDataEntry[] = storage[STORAGE_KEY] ?? [];
-  const blacklist = entries
-    .filter((entry) => entry.checked)
-    .map((entry) => entry.value);
+// update blacklist on storage change
+chrome.storage.onChanged.addListener((changes) => {
+  blacklist.reassign(changes);
+});
 
-  chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
-    if (changeInfo.status === 'loading') {
-      const path = (changeInfo.url || '')
-        .replace(/^https?\:\/\//, '')
-        .replace(/^www\./, '');
+// watch for tabs
+chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
+  if (changeInfo.status === 'loading') {
+    const shouldClose = blacklist.isIncluded(changeInfo.url || '');
 
-      const shouldClose = blacklist.some((entry) => path.startsWith(entry));
-
-      if (shouldClose) {
-        chrome.tabs.remove(tabId);
-      }
+    if (shouldClose) {
+      chrome.tabs.remove(tabId);
     }
-  });
+  }
+});
+
+// make blacklist constructor
+function makeBlacklist() {
+  let blacklist: string[] = [];
+
+  async function reload() {
+    const storage = await chrome.storage.sync.get(STORAGE_KEY);
+    reassign(storage);
+  }
+
+  function reassign(storage: { [name: string]: any }) {
+    const entries: SiteRowsDataEntry[] = storage[STORAGE_KEY] ?? [];
+    const list = entries
+      .filter((entry) => entry.checked)
+      .map((entry) => entry.value);
+
+    blacklist = list;
+  }
+
+  function isIncluded(url: string) {
+    const path = (url || '').replace(/^https?\:\/\//, '').replace(/^www\./, '');
+    const isIncluded = blacklist.some((entry) => path.startsWith(entry));
+
+    return isIncluded;
+  }
+
+  return {
+    reload,
+    reassign,
+    isIncluded,
+  };
 }
