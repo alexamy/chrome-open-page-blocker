00bf494c7de8a7117346a9825622b816f75c8c7e
Aleksandr Chashchin <ac@happynumbers.com>
2025-07-04 15:08:40 +0200

    add makes

--- src/service-worker/blacklist.test.ts
+++ src/service-worker/blacklist.test.ts
@@ -6,8 +6,25 @@ it('makes blacklist', () => {
 
   expect(list.peek).toEqual([]);
 });
-it('reassign list', () => {});
-it('peeks copy of the list', () => {});
-it('reassigns the list', () => {});
+
+it('reassign list', () => {
+  const list = makeBlacklist();
+  list.reassign([{ checked: true, value: 'site' }]);
+
+  expect(list.peek).toEqual(['site']);
+});
+
+it('peeks copy of the list', () => {
+  const list = makeBlacklist();
+  list.reassign([{ checked: true, value: 'site' }]);
+
+  const peek = list.peek;
+  expect(list.peek).toEqual(['site']);
+  peek.push('new-entry');
+
+  expect(list.peek).toEqual(['site']);
+});
+
 it('remove protocol and www from url', () => {});
+
 it('includes only checked entries with a value', () => {});
