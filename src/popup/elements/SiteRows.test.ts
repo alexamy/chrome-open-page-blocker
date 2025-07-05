4b5f76328419fc33467cdbe036c90e8feae21bd6
Aleksandr Chashchin <ac@happynumbers.com>
2025-07-05 14:27:04 +0200

    add checkbox

--- src/popup/elements/SiteRows.test.ts
+++ src/popup/elements/SiteRows.test.ts
@@ -55,6 +55,16 @@ it('adds and focus new row when button is clicked', async () => {
   expect(input.value).toBe('facebook.com');
 });
 
+it('turns on checkbox when adding new rows', async () => {
+  const element = setup();
+
+  await userEvent.click(element.add);
+  await userEvent.keyboard('facebook.com');
+
+  const input = screen.getByShadowRole('checkbox') as HTMLInputElement;
+  expect(input).toBeChecked();
+});
+
 it('removes row', async () => {
   const element = setup(['youtube.com']);
 
